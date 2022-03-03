b43da031b46b78a1646f050011abf1659a706123
_co_cy_ <step2038@mail.ru>
2022-03-03 21:39:51 +0300

    Renaming attribute
    Signed-off-by: _co_cy_ <step2038@mail.ru>

--- database/event.py
+++ database/event.py
@@ -5,13 +5,13 @@ class Event(db.Model):
     id = db.Column(db.INTEGER, index=True, primary_key=True)
 
     image = db.Column(db.Text, nullable=False)
-    annotations = db.Column(db.Text, nullable=False)
+    annotation = db.Column(db.Text, nullable=False)
 
     description = db.Column(db.Text, nullable=False)
 
     is_deleted = db.Column(db.Boolean, nullable=False, default=False)
 
-    def __init__(self, image: str, annotations: str, description: str) -> None:
+    def __init__(self, image: str, annotation: str, description: str) -> None:
         self.image = image
-        self.annotations = annotations
+        self.annotation = annotation
         self.description = description
