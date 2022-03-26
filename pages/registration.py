4466f8edfffc01d8391abe7f3ae6fcd09ef77f38
_co_cy_ <stf2003.kem@gmail.com>
2022-03-26 15:45:20 +0300

    Added logger
    Signed-off-by: _co_cy_ <stf2003.kem@gmail.com>

--- pages/registration.py
+++ pages/registration.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 from forms.registration_user import RegistrationUserForm
+from flask_loguru import logger
 from config import SaveConfig
 from datetime import datetime
 from database import db, user
@@ -11,6 +12,7 @@ blueprint = Blueprint("registration", __name__)
 @blueprint.route("/registration", methods=["GET"])
 @blueprint.route("/reg", methods=["GET"])
 def get_registration():
+    logger.info("Someone went to the user registration page")
     form = RegistrationUserForm()
     return render_template("registration.html", form=form)
 
@@ -18,6 +20,7 @@ def get_registration():
 @blueprint.route("/registration", methods=["POST"])
 @blueprint.route("/reg", methods=["POST"])
 def post_registration():
+    logger.info("Someone sent an application for registration")
     form = RegistrationUserForm()
     if form.validate_on_submit():
         # TODO костыльный метод сохранения изображения мероприятия
@@ -26,6 +29,7 @@ def post_registration():
         filename = f"{round(datetime.utcnow().timestamp() * 1e7)}.{image.filename.split('.')[-1]}"
         img_path = join(SaveConfig.IMAGE_SAVE_DIR, filename)
         image.save(img_path)
+        logger.info("Saved user image to {}", img_path)
 
         new_user = user.User(img_path, form.first_name.data, form.last_name.data,
                              form.nickname.data, form.description.data, form.email.data,
@@ -35,4 +39,5 @@ def post_registration():
         db.session.commit()
         return redirect(url_for('index.index'))
 
+    logger.info("The attempt to registration failed. Errors occurred: {}", form.errors)
     return render_template("registration.html", form=form)
